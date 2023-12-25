c119807a3330cdbcddaac532286120ec911bb8f1
LoveRuKen98 <148434749+LoveRuKen98@users.noreply.github.com>
2023-12-25 20:46:26 +0300

    new

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let randomResult = document.getElementById('randres');
 let table = document.getElementById('myTable');
 const victoryPlate = document.getElementById('victory');
+const victoryText = document.getElementById('victorytext');
 const hider = document.getElementById('hider');
 const hinkali = document.getElementById('hinkali');
 const hinkText = document.getElementById('hinktext');
 const gruzin = document.getElementById('gruzin');
+const continueButton = document.getElementById('continue');
 const players = [];
+const winners = [];
+const firstPlace = document.getElementById('fplace');
+const firstPlaceText = document.getElementById('fplacetext');
+const secondPlace = document.getElementById('splace');
+const secondPlaceText = document.getElementById('splacetext');
+const thirdPlace = document.getElementById('tplace');
+const thirdPlaceText = document.getElementById('tplacetext');
+const winnerImg = document.getElementById('winnerimgcont');
 
 function addPlayer(number, name, life) {
   const row = table.insertRow();
@@ -18,24 +28,26 @@ function addPlayer(number, name, life) {
   const cell3 = row.insertCell();
   cell3.textContent = life;
   cell3.style.border = '1px solid black';
-  players.push({number: number ,name: name, life: life });
+  players.push({ number: number, name: name, life: life });
 }
 
-addPlayer(1,'Лаврентий', 2);
-addPlayer(2,'Шота', 2);
-addPlayer(3,'Бесики', 2);
-addPlayer(4,'Николоз', 2);
-addPlayer(5,'Сандро', 2);
+addPlayer(1, 'Лаврентий Латария', 2);
+addPlayer(2, 'Шота Егутия', 2);
+addPlayer(3, 'Бесики Кирия', 2);
+addPlayer(4, 'Николоз Какабадзе', 2);
+addPlayer(5, 'Сандро Какабадзе', 2);
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
 hinkali.addEventListener('click', () => {
-  let randomID = getRndInteger(1, players.length+1);
+  let randomID = getRndInteger(1, players.length + 1);
   randomResult.value = randomID;
 
   let player = players[randomID - 1];
+
+
 
   if (player.life > 1) {
     player.life--;
@@ -47,39 +59,50 @@ hinkali.addEventListener('click', () => {
       LifeCell.parentElement.style.backgroundColor = 'white';
     }, 150);
   } else {
+    if (players.length < 4 && players.length > 1) {
+      winners.push(players[randomID - 1].name);
+    }
     table.deleteRow(randomID);
     players.splice(randomID - 1, 1);
 
     updatePlayerNumbers(randomID);
-    if (players.length >= 2 )
-    setTimeout(() => {
-      alert(`Участник ${player.name} выбыл! Пока пока :)`);
-    }, 200)
+    if (players.length >= 2)
+      setTimeout(() => {
+        alert(`Участник ${player.name} выбыл! Пока пока :)`);
+      }, 200)
   }
-  if (players.length == 1){
+
+  if (players.length == 1) {
     let winner = players[0].name;
+    table.style.display = 'none';
+    hider.style.backgroundColor = '#454545';
+    hider.style.width = '10000px';
+    hider.style.height = '10000px';
+    victoryPlate.style.display = 'block';
+    victoryText.textContent = `${winner}, с победой! Геймпад твой!`;
+    winners.push(winner);
 
-    if (players.length == 1){
-        let winner = players[0].name;
-        hinkali.style.display = 'none';
-        hinkText.style.display = 'none';
-        gruzin.style.display = 'none';
-        table.style.display = 'none';
-        hider.style.backgroundColor = '#454545';
-        hider.style.width = '10000px';
-        hider.style.height = '10000px';
-          victoryPlate.style.display = 'block';
-          victoryPlate.textContent = `${winner}, с победой! Геймпад твой!`;
-
-      }
   }
+
 });
 
 function updatePlayerNumbers(startIndex) {
-  for (let i = (startIndex-1); i < players.length; i++) {
+  for (let i = (startIndex - 1); i < players.length; i++) {
     players[i].number--;
 
-    const NumberCell = table.rows[i+1].cells[0];
+    const NumberCell = table.rows[i + 1].cells[0];
     NumberCell.textContent = players[i].number;
   }
 }
+
+continueButton.addEventListener('click', () => {
+  victoryPlate.style.display = 'none';
+  hider.style.display = 'none';
+  firstPlace.style.display = 'block';
+  firstPlaceText.textContent = `${winners[2]}`;
+  secondPlace.style.display = 'block';
+  secondPlaceText.textContent = `${winners[1]}`;
+  thirdPlace.style.display = 'block';
+  thirdPlaceText.textContent = `${winners[0]}`;
+  winnerImg.style.display = 'block';
+})
