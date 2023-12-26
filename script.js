76fcad48d6c0095e1980e7443ce063a967bc7df8
LoveRuKen98 <148434749+LoveRuKen98@users.noreply.github.com>
2023-12-26 17:06:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let randomResult = document.getElementById('randres');
+let randomResultSecond = document.getElementById('randrestwo');
 let table = document.getElementById('myTable');
 const victoryPlate = document.getElementById('victory');
 const victoryText = document.getElementById('victorytext');
@@ -34,6 +35,31 @@ addPlayer(2, 'Шота Егутия', 2);
 addPlayer(3, 'Бесики Кирия', 2);
 addPlayer(4, 'Николоз Какабадзе', 2);
 addPlayer(5, 'Сандро Какабадзе', 2);
+addPlayer(6, 'Лаврентий Латария', 2);
+addPlayer(7, 'Шота Егутия', 2);
+addPlayer(8, 'Бесики Кирия', 2);
+addPlayer(9, 'Николоз Какабадзе', 2);
+addPlayer(10, 'Сандро Какабадзе', 2);
+addPlayer(11, 'Лаврентий Латария', 2);
+addPlayer(12, 'Шота Егутия', 2);
+addPlayer(13, 'Бесики Кирия', 2);
+addPlayer(14, 'Николоз Какабадзе', 2);
+addPlayer(15, 'Сандро Какабадзе', 2);
+addPlayer(16, 'Лаврентий Латария', 2);
+addPlayer(17, 'Шота Егутия', 2);
+addPlayer(18, 'Бесики Кирия', 2);
+addPlayer(19, 'Николоз Какабадзе', 2);
+addPlayer(20, 'Сандро Какабадзе', 2);
+addPlayer(21, 'Лаврентий Латария', 2);
+addPlayer(22, 'Шота Егутия', 2);
+addPlayer(23, 'Бесики Кирия', 2);
+addPlayer(24, 'Николоз Какабадзе', 2);
+addPlayer(25, 'Сандро Какабадзе', 2);
+addPlayer(26, 'Лаврентий Латария', 2);
+addPlayer(27, 'Шота Егутия', 2);
+addPlayer(28, 'Бесики Кирия', 2);
+addPlayer(29, 'Николоз Какабадзе', 2);
+addPlayer(30, 'Сандро Какабадзе', 2);
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -42,6 +68,8 @@ function getRndInteger(min, max) {
 hinkali.addEventListener('click', () => {
   let randomID = getRndInteger(1, players.length + 1);
   randomResult.value = randomID;
+  randomResultSecond.value = randomID;
+  hinkali.style.position = 'fixed';
 
   let player = players[randomID - 1];
 
@@ -53,9 +81,11 @@ hinkali.addEventListener('click', () => {
     const LifeCell = table.rows[randomID].cells[2];
     LifeCell.textContent = player.life;
     LifeCell.parentElement.style.backgroundColor = 'red';
+    LifeCell.parentElement.style.transition = 'background-color 1s linear';
     setTimeout(() => {
       LifeCell.parentElement.style.backgroundColor = 'white';
-    }, 150);
+      LifeCell.parentElement.style.transition = 'background-color 1s linear';
+    }, 1000);
   } else {
     if (players.length < 4 && players.length > 1) {
       winners.push(players[randomID - 1].name);
@@ -86,6 +116,11 @@ hinkali.addEventListener('click', () => {
     winners.push(winner);
 
   }
+      const playerRow = table.rows[randomID - 1];
+      const visibleAreaHeight = window.innerHeight - document.querySelector('.randomheader').clientHeight;
+        const scrollY = playerRow.offsetTop - visibleAreaHeight / 2;
+        window.scrollTo(0, scrollY);
+  
 
 });
 
